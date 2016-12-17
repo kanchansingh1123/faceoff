dd23505de53dcf87baf6d457b7939a89266494b1
GaneshPediredla <Ganesh.Pediredla@walkingtree.in>
2016-12-17 17:08:58 +0530

    Updated with tabs view along with login and Register and Forgot Pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,9 @@ import { MyApp } from './app.component';
 import { AboutPage } from '../pages/about/about';
 import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
+import { LoginPage } from '../pages/login/login';
+import { RegisterPage } from '../pages/register/register';
+import { PasswordPage } from '../pages/password/password';
 import { TabsPage } from '../pages/tabs/tabs';
 
 @NgModule({
@@ -12,6 +15,9 @@ import { TabsPage } from '../pages/tabs/tabs';
     AboutPage,
     ContactPage,
     HomePage,
+    LoginPage,
+    RegisterPage,
+    PasswordPage,
     TabsPage
   ],
   imports: [
@@ -23,6 +29,9 @@ import { TabsPage } from '../pages/tabs/tabs';
     AboutPage,
     ContactPage,
     HomePage,
+    LoginPage,
+    RegisterPage,
+    PasswordPage,
     TabsPage
   ],
   providers: [{provide: ErrorHandler, useClass: IonicErrorHandler}]

8c9c14028a3a928d8d5e4751e0975c03e620c9e9
Sucharitha <sucharitha.g@walkingtree.tech>
2016-12-21 20:11:12 +0530

    Comitted by including Diet , Workout,Appointments activation changes in schedule component

--- src/pages/schedule/schedule.ts
+++ src/pages/schedule/schedule.ts
@@ -6,17 +6,74 @@ import { ModalPage } from './modal-page/modal-page';
 
 
 @Component({
-  selector: 'page-schedule',
+  selector: 'page-schedulerList',
   templateUrl: 'schedule.html'
 })
 export class SchedulePage {
 
-  constructor(public navCtrl: NavController,public alertCtrl: AlertController,public modalCtrl: ModalController) {
+  public isRemainder:boolean=true;
+  public isDiet:boolean=false;
+  public isAppointments:boolean=false;
+  public isWorkout:boolean=false;
+  public doctors:any;
 
+  constructor(public navCtrl: NavController,public alertCtrl: AlertController,public modalCtrl: ModalController) {
+    this.doctors = [
+	  {
+	      Name:"Dr.Shaw",
+	      Graduation:"MBBS",
+	      PostGraduation:"MD",
+	      Specialization:"General Medicine DNB",
+	      Experience:"5",
+	      Fees:"500",
+	      StartTiming:"MON-TUE 11:00AM-1:00PM",
+	      EndTiming:"THU-SAT 7:00PM-9:00PM",
+	      Rating:"90%",
+	      image:"assets/images/doctor1.png"
+	 }  ,
+	  {
+	      Name:"Dr.Shaw123",
+	      Graduation:"MBBS",
+	      PostGraduation:"MD",
+	      Specialization:"General Medicine DNB",
+	      Experience:"5",
+	      Fees:"500",
+	      StartTiming:"MON-TUE 11:00AM-1:00PM",
+	      EndTiming:"THU-SAT 7:00PM-9:00PM",
+	      Rating:"85%",
+	      image:"assets/images/doctor2.png"
+	  }
+  ];
   }
-  showRemainderPopup() {
-    let modal = this.modalCtrl.create(ModalPage);
-    modal.present();
+
+
+showRemainderPopup() {
+  let modal = this.modalCtrl.create(ModalPage);
+  modal.present();
+}
+showAppointments(){
+this.isRemainder=false;
+this.isAppointments=true;
+this.isDiet=false;
+this.isWorkout=false;
+}
+showRemainders(){
+this.isRemainder=true;
+this.isAppointments=false;
+this.isDiet=false;
+this.isWorkout=false;
+}
+showDietDetails(){
+this.isRemainder=false;
+this.isAppointments=false;
+this.isDiet=true;
+this.isWorkout=false;
+}
+showWorkoutPlan(){
+  this.isWorkout=true;
+  this.isRemainder=false;
+  this.isAppointments=false;
+  this.isDiet=false;
 }
 
 }

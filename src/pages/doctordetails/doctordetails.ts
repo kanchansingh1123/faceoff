dfe0dfbd01a939108759e79a597c7e1947f8a224
Anil K <anil.kondepudi@walkingtree.in>
2016-12-21 15:52:44 +0530

    integrated the notifications and search

--- src/pages/doctordetails/doctordetails.ts
+++ src/pages/doctordetails/doctordetails.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 
-import { NavController } from 'ionic-angular';
+import { NavController,NavParams } from 'ionic-angular';
+import { ActionSheetController } from 'ionic-angular';
 
 @Component({
   selector: 'page-doctor-details',
@@ -8,7 +9,9 @@ import { NavController } from 'ionic-angular';
 })
 export class DoctorDetailsPage {
 public doctors:any;
-  constructor(public navCtrl: NavController) {
+public serviceName: any;
+  constructor(public navCtrl: NavController,public params: NavParams,public actionSheetCtrl: ActionSheetController) {
+  	this.serviceName = this.params.get('serviceName');
   	this.doctors = [
 	  {
 	      Name:"Dr.Shaw",
@@ -35,6 +38,83 @@ public doctors:any;
 	      image:"assets/images/doctor2.png"
 	  }
   ];
+
+
+  };
+
+   bookNow(){
+   let actionSheet = this.actionSheetCtrl.create({
+      buttons: [
+        {
+          text: 'M',
+          handler: () => {
+            console.log('Destructive clicked');
+          },
+          cssClass:'schedule-day-button-enable'
+        },
+        {
+          text: 'T',
+          handler: () => {
+            console.log('Destructive clicked');
+          },
+          cssClass:'schedule-day-button-disable'
+        },
+        {
+          text: 'W',
+          handler: () => {
+            console.log('Destructive clicked');
+          },
+          cssClass:'schedule-day-button-disable'
+        },
+        {
+          text: 'T',
+          handler: () => {
+            console.log('Destructive clicked');
+          },
+          cssClass:'schedule-day-button-disable'
+        },
+        {
+          text: 'F',
+          handler: () => {
+            console.log('Destructive clicked');
+          },
+          cssClass:'schedule-day-button-disable'
+        },
+        {
+          text: 'S',
+          handler: () => {
+            console.log('Destructive clicked');
+          },
+          cssClass:'schedule-day-button-disable'
+        },
+        {
+         text: '23 Dec',
+         cssClass:'date-button'
+        },
+        {
+         text: '11:00 AM - 12:00 PM',
+         cssClass: "timings-button"
+        },
+        {
+         text: '1:00 PM - 2:00 PM',
+         cssClass: "timings-button"
+        },
+        {
+         text: '3:00 PM - 4:00 PM',
+         cssClass: "timings-button"
+        },
+        {
+         text: '5:00 PM - 6:00 PM',
+         cssClass: "timings-button"
+        },
+        {
+	      text:"BOOK NOW",
+	      cssClass: 'book-now'
+	    }
+      ],
+      enableBackdropDismiss: false
+    });
+    actionSheet.present();
   }
   
 }
--- src/pages/header/header.ts
+++ src/pages/header/header.ts
@@ -1,15 +1,19 @@
 import { Component } from '@angular/core';
 
 import { NavController } from 'ionic-angular';
+import { SearchPage } from '../search/search';
+import { NotificationPage } from '../notification/notification';
 
 @Component({
   selector: 'header-bar',
   templateUrl: 'header.html'
 })
 export class HeaderBarPage {
-
+	searchPage: any = SearchPage;
+	notificationPage: any = NotificationPage;
   constructor(public navCtrl: NavController) {
 
   }
 
 }
+

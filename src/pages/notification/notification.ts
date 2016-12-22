9717e21cc47da79d857e4e514a91f3e4e6671c88
Anil K <anil.kondepudi@walkingtree.in>
2016-12-23 00:11:28 +0530

    may be last

--- src/pages/notification/notification.ts
+++ src/pages/notification/notification.ts
@@ -25,6 +25,86 @@ public doctors:any;
 		]
   }
 
+  rateNow(){
+ 	  let alert = this.alertCtrl.create({
+    title: '<h4 style="text-align: center;">Please rate your Experience with <a>Dr.Shaw</a></h4><hr>',
+      buttons: [
+        {
+	         text: "1",
+	         handler: () => {
+	           this.ratingRange(alert,  0);
+	           return false;
+	         },
+	         cssClass:'ratenow1'
+        },
+        {
+	         text: 2,
+	         handler: () => {
+	         
+	         this.ratingRange(alert,  1);
+	         console.log(alert);
+	           return false;
+	         },
+	         cssClass:'ratenow2'
+        },
+        {
+	        text:3,
+	        handler: () => {
+	          this.ratingRange(alert,  2);
+	          return false;
+	        },
+	        cssClass:'ratenow3'
+      	},
+      	{
+	        text:4,
+	        handler: () => {
+	          this.ratingRange(alert,  3);
+	          return false;
+	        },
+	        cssClass:'ratenow4'
+      	},
+      	{
+	        text:5,
+	        handler: () => {
+	          this.ratingRange(alert,  4);
+	          return false;
+	        },
+	        cssClass:'ratenow5'
+      	},
+      	{
+	        text:"Submit",
+	        handler: () => {
+	          this.thanksAlert();
+	          return true;
+	        },
+	        cssClass:'ratenow-submit'
+      	},
+      	{
+	        text:"Not now",
+	        handler: () => {
+	          return true;
+	        },
+	        cssClass:'ratenow-not'
+      	}
+      ]
+    });
+    alert.present();
+  }
+  
+   ratingRange(alert,  index){
+     for(let i = 0 ; i < 5; i++ ){
+                 let count = i+1;
+                 if(i <= index){
+                   alert.data.buttons[i].text="";
+	               alert.data.buttons[i].cssClass = "ratenow"+count + " heartbeatrate hartwidth";
+                 }
+                 else{
+                   alert.data.buttons[i].text= i+1;
+	               alert.data.buttons[i].cssClass ="ratenow"+count;
+                 }
+	         }
+   }
+
   thanksAlert() {
     let alert = this.alertCtrl.create({
       title: '<h4>Thanks for your Feedback.</h4>',
